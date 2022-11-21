c83926fdc2a866552c28f0000a81009dde6b3980
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-11-21 23:24:15 +0300

    arithmetic using math methods

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1=62;
+let num2=1;
+let multiply=num1*num2;
+const random= Math.floor(Math.random() * 10) + 1;
+let num3=4;
+let num4=5;
+let mod=num3%num4;
+let max= Math.max(4,14,6,1,2,20);
+console.log(max)
